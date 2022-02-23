da6899de9284b803b51dcf0f12837628676fedc6
Kruzen <lengeryhit@mail.ru>
2022-02-23 21:44:38 +0400

    Initial code

---
+++ UQM_PalEd/UQM_PalEd/MainForm.cpp
@@ -0,0 +1,15 @@
+#include "MainForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	UQMPalEd::MainForm form;
+	Application::Run(% form);
+}
---
+++ UQM_PalEd/UQM_PalEd/MainForm.h
@@ -0,0 +1,72 @@
+#pragma once
+
+namespace UQMPalEd {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(800, 372);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
